4d38f07a421e2f54eb370c93e32b3db130456314
petk1234 <petkun8034@gmail.com>
2021-08-26 23:25:15 +0300

    Final commit

--- src/index.js
+++ src/index.js
@@ -1,41 +1,16 @@
-const colors = [
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
-  ];
-
-const refs = {
-    bodyEl: document.querySelector('body'),
-    startButton: document.querySelector("[data-action='start']"),
-    stopButton: document.querySelector("[data-action='stop']"),
-}
-console.log(refs.bodyEl);
-console.log(refs.startButton);
-console.log(refs.stopButton);
-
-let idd;
-
-const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
-const colorChange = e =>{
-    refs.startButton.disabled = true;
-    refs.stopButton.disabled = false;
-    const qwe = () =>{
-        refs.bodyEl.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length)];
-    }
-
-    idd = window.setInterval(qwe, 1000);
-    console.log('Start');
-}
-const stopColorChange = () =>{
-    refs.startButton.disabled = false;
-    refs.stopButton.disabled = true;
-    clearInterval(idd);
-    console.log('Stop');
-}
-refs.startButton.addEventListener('click', colorChange);
-refs.stopButton.addEventListener('click', stopColorChange);
+const delay = ms => {
+    const promise = new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(ms);
+        }, ms);
+    });
+    console.log('delay +', ms);
+    return promise;
+};
+  
+  const logger = time => console.log(`Resolved after ${time}ms`);
+  
+  // Вызовы функции для проверки
+  delay(2000).then(logger); // Resolved after 2000ms
+  delay(1000).then(logger); // Resolved after 1000ms
+  delay(1500).then(logger); // Resolved after 1500ms
